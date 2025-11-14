9419dec94a988bc9505232d79c2adfeea21a96d2
Mohamed Madian <mosamehmadian88@gmail.com>
2025-11-14 20:20:47 +0200

    Update 11_part_to_whole_analysis.sql

--- scripts/11_part_to_whole_analysis.sql
+++ scripts/11_part_to_whole_analysis.sql
@@ -16,10 +16,10 @@ SQL Functions Used:
 WITH category_sales AS (
     SELECT
         p.category,
-        SUM(f.sales_amount) AS total_sales
-    FROM gold.fact_sales f
+        SUM(s.sales_amount) AS total_sales
+    FROM gold.fact_sales s
     LEFT JOIN gold.dim_products p
-        ON p.product_key = f.product_key
+        ON p.product_key = s.product_key
     GROUP BY p.category
 )
 SELECT
@@ -28,4 +28,5 @@ SELECT
     SUM(total_sales) OVER () AS overall_sales,
     ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
 FROM category_sales
+
 ORDER BY total_sales DESC;
